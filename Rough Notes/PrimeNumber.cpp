f2e8f308a0b8e10817a4204add7e6d75e4547ecb
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-02-04 10:18:30 +0530

    change

---
+++ Rough Notes/PrimeNumber.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, flag = 0;
+    cout<<"Enter number you want to check prime:- ";
+    cin>>n;
+
+    for(int i=2; i<n/2; i++)
+    {
+        if(n%i == 0)
+        {
+            flag=0;
+            break;
+        }
+        flag++;
+    }
+    if(flag == 0)
+        cout<<endl<<"Number is not prime.";
+    else
+        cout<<endl<<"Number is prime";
+
+    return 0;
+}
