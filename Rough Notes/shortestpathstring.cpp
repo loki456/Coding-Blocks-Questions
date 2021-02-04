99c2112090895e4a5ee055300dbd234691f95111
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-02-04 11:14:01 +0530

    change

---
+++ Rough Notes/shortestpathstring.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+    int x, y;
+
+    while(ch!='\n')
+    {
+        ch = cin.get();
+        if(ch == 'n')
+            x++;
+        if(ch == 's')
+            x--;
+        if(ch == 'e')
+            y++;
+        if(ch == 'w')
+            y--;        
+    }
+    cout<<endl<<abs(x)<<endl<<abs(y);
+    return 0;
+}
---
+++ Rough Notes/von_neuman_loves_binary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    while(n--)
+    {
+        int a;
+        cin>>a;
+        while(a>0)
+        {
+            int 
+        }
+    }
+    return 0;
+}
