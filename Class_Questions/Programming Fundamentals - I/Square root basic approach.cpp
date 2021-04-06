7d5f7c66144f9c7df7171a01a4af58cee5531753
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-04-06 18:34:09 +0530

    Create Square root basic approach.cpp

---
+++ Class_Questions/Programming Fundamentals - I/Square root basic approach.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    float i=0;
+    for(i=0; i*i<=n; i++){
+
+    }
+    cout<<endl<<i-1.0;
+}
